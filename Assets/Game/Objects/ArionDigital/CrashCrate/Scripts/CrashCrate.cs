bde4b85630291431cc7f4a97c52cb15f6412ebaf
csyuet <csyuet@connect.hku.hk>
2025-04-09 21:31:03 +0800

    Add Tank assets and Crate assets

---
+++ Assets/Game/Objects/ArionDigital/CrashCrate/Scripts/CrashCrate.cs
@@ -0,0 +1,41 @@
+﻿namespace ArionDigital
+{
+    using UnityEngine;
+
+    public class CrashCrate : MonoBehaviour
+    {
+        [Header("Whole Create")]
+        public MeshRenderer wholeCrate;
+        public BoxCollider boxCollider;
+        [Header("Fractured Create")]
+        public GameObject fracturedCrate;
+        [Header("Audio")]
+        public AudioSource crashAudioClip;
+
+        [Header("Destory Time")]
+        public float timeToDestory;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            wholeCrate.enabled = false;
+            boxCollider.enabled = false;
+            fracturedCrate.SetActive(true);
+            crashAudioClip.Play();
+            Invoke("DestroySelf", timeToDestory);
+        
+        }
+
+        [ContextMenu("Test")]
+        public void Test()
+        {
+            wholeCrate.enabled = false;
+            boxCollider.enabled = false;
+            fracturedCrate.SetActive(true);
+        }
+
+        void DestroySelf()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
